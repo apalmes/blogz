21f43f1d35a0e6e83de2394699b79eb7bb3360a3
asclepiusgal <jeanna.e.clark@gmail.com>
2017-07-11 10:03:41 -0700

    code review

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-from flask import Flask, request, redirect, render_template, flash, session, url_for
+from flask import Flask, request, redirect, render_template, flash, session
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from password_hash import make_pw_hash, check_pw_hash 
+from password_hash import make_pw_hash, check_pw_hash
 
-app= Flask(__name__)
+app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:hello@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'Super_Secret_Key'
+
 
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -25,8 +26,8 @@ class Blog(db.Model):
         self.post_date = post_date
         self.owner = owner
 
-class User(db.Model):
 
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), unique=True)
     pw_hash = db.Column(db.String(120))
@@ -49,7 +50,7 @@ def index():
     users = User.query.all()
     page_title = 'Blogz'
     return render_template('index.html', users=users, page_title=page_title)
-    
+
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
@@ -60,26 +61,27 @@ def login():
         if not user:
             flash('Invalid Username', 'error')
             return redirect('/login')
-        if user and check_pw_hash(password, user.pw_hash) == False:
+        if user and not check_pw_hash(password, user.pw_hash):
             flash('Invalid Password', 'error')
         if user and check_pw_hash(password, user.pw_hash):
             session['username'] = username
             return redirect('/newpost')
 
-    page_title = 'Blogz'   
-        
+    page_title = 'Blogz'
+
     return render_template('login.html', page_title=page_title)
 
-    
+
 @app.route('/signup', methods=['POST', 'GET'])
 def signup():
-    page_title='Blogz'
+    page_title = 'Blogz'
 
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         verify = request.form['verify']
 
+        # TODO: this error handling can be replaced with flash messages
         username_error = ''
         password_error = ''
         verify_error = ''
@@ -91,21 +93,21 @@ def signup():
 
         if (len(username) < 3) or (len(username) > 20) or username == "":
             username_error = "That is not a valid username"
-        
+
         for char in username:
             if char == " ":
                 username_error = "That is not a valid username"
-            
+
         if (len(password) < 3) or (len(password) > 20) or password == "":
             password_error = "That is not a valid password"
-    
+
         for char in password:
             if char == " ":
                 password_error = 'That is not a valid password'
-    
+
         if (verify != password) or verify == '':
-            verify_error="The passwords do not match"
-    
+            verify_error = "The passwords do not match"
+
         if not username_error and not password_error and not verify_error and not existing_user:
             new_user = User(username, password)
             db.session.add(new_user)
@@ -114,10 +116,11 @@ def signup():
             return redirect('/newpost')
 
         else:
-            return render_template('signup.html', title='Sign-up', username_error=username_error, password_error=password_error, verify_error=verify_error, page_title=page_title)
+            return render_template('signup.html', title='Sign-up', username_error=username_error,
+                                   password_error=password_error, verify_error=verify_error, page_title=page_title)
 
     return render_template('signup.html', page_title=page_title)
-    
+
 
 @app.route('/logout')
 def logout():
@@ -128,25 +131,26 @@ def logout():
 @app.route('/blog', methods=['POST', 'GET'])
 def blogs():
     users = User.query.all()
+    # TODO: you can query the blog entries by date descending
     blog_entries = Blog.query.all()
     page_title = 'Blogz'
     main_title = 'Blog Posts'
-    
+
     if request.method == 'GET':
         if 'id' in request.args:
             blog_id = request.args.get('id')
-            blog_content= Blog.query.get(blog_id)
+            blog_content = Blog.query.get(blog_id)
             owner = blog_content.owner
             return render_template('post.html', blog_content=blog_content, page_title=page_title, owner=owner)
-        
+
         if 'user' in request.args:
             username = request.args.get('user')
             owner = User.query.filter_by(username=username).first()
             user_post = Blog.query.filter_by(owner=owner).all()
             return render_template('singleUser.html', user_post=user_post, owner=owner, page_title=page_title)
 
-
-    return render_template('blog.html', main_title=main_title, page_title=page_title, blog_entries=blog_entries, users=users)
+    return render_template('blog.html', main_title=main_title, page_title=page_title, blog_entries=blog_entries,
+                           users=users)
 
 
 @app.route('/newpost', methods=['POST', 'GET'])
@@ -154,7 +158,7 @@ def newpost():
     owner = User.query.filter_by(username=session['username']).first()
     if request.method == 'POST':
         title = request.form['title']
-        body = request.form ['body']
+        body = request.form['body']
 
         if (title == '') or (body == ''):
             flash('Oops, did you forget something...?', 'error')
@@ -164,7 +168,6 @@ def newpost():
             db.session.commit()
             new_blog = Blog.query.order_by('-id').first()
             new_blog_redirect = new_blog.id
-            users = User.query.all()
             return redirect('/blog?id={0}'.format(new_blog_redirect))
 
     page_title = 'Blogz'
--- password_hash.py
+++ password_hash.py
@@ -10,13 +10,13 @@ def make_salt():
 def make_pw_hash(password, salt=None):
     if not salt:
         salt = make_salt()
-    hash = hashlib.sha256(str.encode(password + salt)).hexdigest()
-    return '{0},{1}'.format(hash, salt)
+    hashed = hashlib.sha256(str.encode(password + salt)).hexdigest()
+    return '{0},{1}'.format(hashed, salt)
 
 
-def check_pw_hash(password, hash):
-    salt = hash.split(',')[1]
-    if make_pw_hash(password, salt) == hash:
+def check_pw_hash(password, hashed):
+    salt = hashed.split(',')[1]
+    if make_pw_hash(password, salt) == hashed:
         return True
 
     return False
